d132428fcd22b18d0e7e5ed2c01d95a1e8da881b
Adriano S <adrianossfontes@gmail.com>
2024-12-19 14:08:18 -0300

    configuração de PORT para o Render

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import * as dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes'
 import studentRoutes from './routes/students.routes'
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
@@ -12,7 +15,7 @@ app.use(cors());
 app.use(authRoutes)
 app.use(studentRoutes)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("🚀 Server ready at: http://localhost:3000");
 });
 
